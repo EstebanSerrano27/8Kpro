78c99786ce1dc6105d01a9eb628dfd972b7d74f1
Luis Esteban Serrano <luis.serrano@biossmann.com>
2023-08-28 15:24:29 -0600

    Commit inicial

---
+++ 8Kpro/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8Kpro
+{
+    public partial class Form1 : Form
+    {
+		//private DeckLinkOutputDevice m_selectedDevice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
