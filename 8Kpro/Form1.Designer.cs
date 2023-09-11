b4fdcb83d9cbc173ad93260c4aa579ba60d4b438
Luis Esteban Serrano <luis.serrano@biossmann.com>
2023-09-11 16:50:16 -0600

    Obtención de los puertos de las tarjetas BlackMagic

--- 8Kpro/Form1.Designer.cs
+++ 8Kpro/Form1.Designer.cs
@@ -29,45 +29,169 @@ namespace _8Kpro
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxVideoFormat = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Port1 = new System.Windows.Forms.Label();
+            this.Port2 = new System.Windows.Forms.Label();
+            this.Port4 = new System.Windows.Forms.Label();
+            this.Port3 = new System.Windows.Forms.Label();
+            this.preview1 = new System.Windows.Forms.PictureBox();
+            this.preview2 = new System.Windows.Forms.PictureBox();
+            this.preview3 = new System.Windows.Forms.PictureBox();
+            this.preview4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.preview1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preview2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preview3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preview4)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // comboBox2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Playback Device";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(106, 48);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(279, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
-            // comboBox1
+            // label2
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(106, 6);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(279, 21);
-            this.comboBox1.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input Connection";
+            // 
+            // comboBoxVideoFormat
+            // 
+            this.comboBoxVideoFormat.FormattingEnabled = true;
+            this.comboBoxVideoFormat.Location = new System.Drawing.Point(106, 96);
+            this.comboBoxVideoFormat.Name = "comboBoxVideoFormat";
+            this.comboBoxVideoFormat.Size = new System.Drawing.Size(279, 21);
+            this.comboBoxVideoFormat.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Video Format";
+            // 
+            // Port1
+            // 
+            this.Port1.AutoSize = true;
+            this.Port1.Location = new System.Drawing.Point(12, 161);
+            this.Port1.Name = "Port1";
+            this.Port1.Size = new System.Drawing.Size(32, 13);
+            this.Port1.TabIndex = 6;
+            this.Port1.Text = "Port1";
+            // 
+            // Port2
+            // 
+            this.Port2.AutoSize = true;
+            this.Port2.Location = new System.Drawing.Point(353, 161);
+            this.Port2.Name = "Port2";
+            this.Port2.Size = new System.Drawing.Size(32, 13);
+            this.Port2.TabIndex = 7;
+            this.Port2.Text = "Port2";
+            // 
+            // Port4
+            // 
+            this.Port4.AutoSize = true;
+            this.Port4.Location = new System.Drawing.Point(353, 371);
+            this.Port4.Name = "Port4";
+            this.Port4.Size = new System.Drawing.Size(32, 13);
+            this.Port4.TabIndex = 9;
+            this.Port4.Text = "Port4";
+            // 
+            // Port3
+            // 
+            this.Port3.AutoSize = true;
+            this.Port3.Location = new System.Drawing.Point(12, 371);
+            this.Port3.Name = "Port3";
+            this.Port3.Size = new System.Drawing.Size(32, 13);
+            this.Port3.TabIndex = 8;
+            this.Port3.Text = "Port3";
+            // 
+            // preview1
+            // 
+            this.preview1.Location = new System.Drawing.Point(15, 177);
+            this.preview1.Name = "preview1";
+            this.preview1.Size = new System.Drawing.Size(289, 169);
+            this.preview1.TabIndex = 10;
+            this.preview1.TabStop = false;
+            // 
+            // preview2
+            // 
+            this.preview2.Location = new System.Drawing.Point(356, 177);
+            this.preview2.Name = "preview2";
+            this.preview2.Size = new System.Drawing.Size(289, 169);
+            this.preview2.TabIndex = 11;
+            this.preview2.TabStop = false;
+            // 
+            // preview3
+            // 
+            this.preview3.Location = new System.Drawing.Point(356, 387);
+            this.preview3.Name = "preview3";
+            this.preview3.Size = new System.Drawing.Size(289, 169);
+            this.preview3.TabIndex = 13;
+            this.preview3.TabStop = false;
+            // 
+            // preview4
+            // 
+            this.preview4.Location = new System.Drawing.Point(15, 387);
+            this.preview4.Name = "preview4";
+            this.preview4.Size = new System.Drawing.Size(289, 169);
+            this.preview4.TabIndex = 12;
+            this.preview4.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(960, 652);
+            this.Controls.Add(this.preview3);
+            this.Controls.Add(this.preview4);
+            this.Controls.Add(this.preview2);
+            this.Controls.Add(this.preview1);
+            this.Controls.Add(this.Port4);
+            this.Controls.Add(this.Port3);
+            this.Controls.Add(this.Port2);
+            this.Controls.Add(this.Port1);
+            this.Controls.Add(this.comboBoxVideoFormat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.preview1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preview2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preview3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preview4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxVideoFormat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Port1;
+        private System.Windows.Forms.Label Port2;
+        private System.Windows.Forms.Label Port4;
+        private System.Windows.Forms.Label Port3;
+        private System.Windows.Forms.PictureBox preview1;
+        private System.Windows.Forms.PictureBox preview2;
+        private System.Windows.Forms.PictureBox preview3;
+        private System.Windows.Forms.PictureBox preview4;
     }
 }
 
--- 8Kpro/Form1.cs
+++ 8Kpro/Form1.cs
@@ -1,21 +1,198 @@
-﻿using System;
+﻿using SignalGenCSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using DeckLinkAPI;
+using CapturePreviewCSharp;
 
 namespace _8Kpro
 {
     public partial class Form1 : Form
     {
-		//private DeckLinkOutputDevice m_selectedDevice;
+        private Thread m_deckLinkMainThread;
+        private readonly EventWaitHandle m_applicationCloseWaitHandle;
+
+        private DeckLinkDevice m_selectedDevice;
+        private DeckLinkDeviceDiscovery m_deckLinkDeviceDiscovery;
+        private ProfileCallback m_profileCallback;
+
+        private DeckLinkDevice[] deckLinkPortDevices;
+
+        private int Infz = 8,
+                    vrCard = 0;
+
+        private IReadOnlyList<StringObjectPair<_BMDVideoConnection>> kInputConnectionList = new List<StringObjectPair<_BMDVideoConnection>>
+        {
+            new StringObjectPair<_BMDVideoConnection>("SDI",            _BMDVideoConnection.bmdVideoConnectionSDI),
+            new StringObjectPair<_BMDVideoConnection>("HDMI",           _BMDVideoConnection.bmdVideoConnectionHDMI),
+            new StringObjectPair<_BMDVideoConnection>("Optical SDI",    _BMDVideoConnection.bmdVideoConnectionOpticalSDI),
+            new StringObjectPair<_BMDVideoConnection>("Component",      _BMDVideoConnection.bmdVideoConnectionComponent),
+            new StringObjectPair<_BMDVideoConnection>("Composite",      _BMDVideoConnection.bmdVideoConnectionComposite),
+            new StringObjectPair<_BMDVideoConnection>("S-Video",        _BMDVideoConnection.bmdVideoConnectionSVideo)
+        };
+
+        private IReadOnlyList<StringObjectPair<_BMDTimecodeFormat>> kTimecodeFormatList = new List<StringObjectPair<_BMDTimecodeFormat>>
+        {
+            new StringObjectPair<_BMDTimecodeFormat>("VITC Field 1",    _BMDTimecodeFormat.bmdTimecodeVITC),
+            new StringObjectPair<_BMDTimecodeFormat>("VITC Field 2",    _BMDTimecodeFormat.bmdTimecodeVITCField2),
+            new StringObjectPair<_BMDTimecodeFormat>("RP188 VITC1",     _BMDTimecodeFormat.bmdTimecodeRP188VITC1),
+            new StringObjectPair<_BMDTimecodeFormat>("RP188 VITC2",     _BMDTimecodeFormat.bmdTimecodeRP188VITC2),
+            new StringObjectPair<_BMDTimecodeFormat>("RP188 LTC",       _BMDTimecodeFormat.bmdTimecodeRP188LTC),
+            new StringObjectPair<_BMDTimecodeFormat>("RP188 HFRTC",     _BMDTimecodeFormat.bmdTimecodeRP188HighFrameRate),
+        };
+
         public Form1()
         {
             InitializeComponent();
+            m_applicationCloseWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            m_deckLinkMainThread = new Thread(() => DeckLinkMainThread());
+            m_deckLinkMainThread.Start();
+        }
+        
+        private void DeckLinkMainThread()
+        {
+            m_deckLinkDeviceDiscovery = new DeckLinkDeviceDiscovery();
+            m_deckLinkDeviceDiscovery.DeviceArrived += AddDevice;
+            m_deckLinkDeviceDiscovery.DeviceRemoved += RemoveDevice;
+            m_deckLinkDeviceDiscovery.Enable();
+
+            m_applicationCloseWaitHandle.WaitOne();
+
+            m_deckLinkDeviceDiscovery.Disable();
+            m_deckLinkDeviceDiscovery.DeviceArrived -= AddDevice;
+            m_deckLinkDeviceDiscovery.DeviceRemoved -= RemoveDevice;
+        }
+
+        public void AddDevice(object sender, DeckLinkDiscoveryEventArgs e)
+        {
+            try
+            {
+                DeckLinkDevice device = new DeckLinkDevice(e.deckLink, m_profileCallback);
+                var deviceActive = device.IsActive;
+
+                UpdatePorts(device);
+                
+                if (vrCard == Infz)
+                {
+                    CardStartup(deckLinkPortDevices);
+                }
+            }
+            catch (DeckLinkInputInvalidException)
+            {
+                // Device does not support input interface
+            }
+        }
+
+        public void RemoveDevice(object sender, DeckLinkDiscoveryEventArgs e)
+        {
+            if (m_selectedDevice?.DeckLink == e.deckLink)
+            {
+                //stop capture and disable input
+                m_selectedDevice.StopCapture();
+            }
+        }
+                        
+        struct StringObjectPair<T>
+        {
+            public StringObjectPair(string name, T value)
+            {
+                Name = name;
+                Value = value;
+            }
+            public string Name { get; }
+            public T Value { get; set; }
+            public override string ToString() => Name;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if ((m_selectedDevice == null) || (comboBox2.SelectedIndex < 0))
+                return;
+
+            var videoInputConnection = (_BMDVideoConnection)comboBox2.SelectedValue;
+            new MTAAction(() => m_selectedDevice.CurrentVideoInputConnection = videoInputConnection);
+
+            UpdateComboVideoModes();
+        }
+
+        private void UpdateComboVideoModes()
+        {
+            if (m_selectedDevice == null)
+                return;
+
+            List<DisplayModeEntry> displayModeEntries = new List<DisplayModeEntry>();
+
+            new MTAAction(() =>
+            {
+                foreach (IDeckLinkDisplayMode displayMode in m_selectedDevice.DisplayModes)
+                {
+                    displayModeEntries.Add(new DisplayModeEntry(displayMode));
+                }
+            });
+
+            // Bind display mode list to combo 
+            comboBoxVideoFormat.DataSource = displayModeEntries;
+            comboBoxVideoFormat.DisplayMember = "DisplayString";
+            comboBoxVideoFormat.ValueMember = "Value";
+
+            if (comboBoxVideoFormat.Items.Count > 0)
+                comboBoxVideoFormat.SelectedIndex = 0;
+        }
+
+        struct DisplayModeEntry
+        /// Used for putting the BMDDisplayMode value into the video format combo
+        {
+            private readonly string m_displayString;
+
+            public DisplayModeEntry(IDeckLinkDisplayMode displayMode)
+            {
+                Value = displayMode.GetDisplayMode();
+                displayMode.GetName(out m_displayString);
+            }
+
+            public _BMDDisplayMode Value { get; set; }
+            public string DisplayString { get => m_displayString; }
+        }
+
+        private void UpdatePorts(DeckLinkDevice device)
+        {
+            vrCard++;
+            Console.WriteLine(device.DisplayName.ToString() + " " + vrCard);
+
+            if (deckLinkPortDevices == null)
+            {
+                deckLinkPortDevices = new DeckLinkDevice[] { device };
+            }
+            else
+            {
+                DeckLinkDevice[] newDevicesArray = new DeckLinkDevice[deckLinkPortDevices.Length + 1];
+                deckLinkPortDevices.CopyTo(newDevicesArray, 0);
+                newDevicesArray[newDevicesArray.Length - 1] = device;
+                deckLinkPortDevices = newDevicesArray;
+            }
+        }
+
+        private void CardStartup(DeckLinkDevice[] deckLinkPortDevices)
+        {
+            this.Invoke((Action)(() =>
+            {
+                Port1.Text = deckLinkPortDevices[0].DisplayName.ToString();
+                Port2.Text = deckLinkPortDevices[1].DisplayName.ToString();
+                Port3.Text = deckLinkPortDevices[2].DisplayName.ToString();
+                Port4.Text = deckLinkPortDevices[3].DisplayName.ToString();
+            }));
         }
     }
 }
